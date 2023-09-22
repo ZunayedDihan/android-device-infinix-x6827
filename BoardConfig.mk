dc7d249fc9bb5255a857cf376ed57fd5624ad979
ZunayedDihan <zunayeddihan@gmail.com>
2023-09-23 01:01:56 +0600

    let check 1st

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/Image.gz
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 
