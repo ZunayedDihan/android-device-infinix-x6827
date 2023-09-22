ad2009e00777f137dacf9ab96aff2991b1773300
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2023-09-23 02:02:49 +0600

    Update and rename lineage_fleur.mk to aosp_fleur.mk

--- aosp_fleur.mk
+++ aosp_fleur.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineageos stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineag/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
